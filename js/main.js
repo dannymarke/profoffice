2c093c536f70ab0957df51a2ae149900fa928583
dannymarke <skylark@iol.it>
2012-10-22 15:50:53 +0200

    riorganizzazione ad oggetti delle funzioni js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
+var po = {};
+po.functions = {};
+
+po.functions.img_seo_placeholder = function(){
+  jQuery('.img_seo_placeholder').each(function(){
+    var $this = jQuery(this);
+    $this.parent('.dyn_bg_wr').css({'background-image' : 'url(' + $this.attr('src') + ')'});
+    $this.remove();
+  });
+};
+
+po.functions.init = function(){
+  po.functions.img_seo_placeholder();
+};
+
 jQuery(document).bind('ready',function(){
-    jQuery('.img_seo_placeholder').each(function(){
-        var $this = jQuery(this);
-        $this.parent('.dyn_bg_wr').css({'background-image' : 'url(' + $this.attr('src') + ')'});
-        $this.remove();
+  jQuery.each(po.functions, function() {
+      this(document); // context
     });
 });
