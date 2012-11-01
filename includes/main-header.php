ae4bcdf56e95e8e68c544149f746d0000f38c7f1
dannymarke <skylark@iol.it>
2012-11-01 20:08:43 +0100

    assegnata classe current alla lingua corrente in modo dinamico

--- includes/main-header.php
+++ includes/main-header.php
@@ -2,7 +2,16 @@
             <header id="main_header">
                 <h1 id="main_logo_wr"><a href="/" id="main_logo" class="ir">Prof Office</a></h1>
                 <ul id="main_lang_selector">
-                    <li><a href="/index.php?lang=it">ITA</a></li>
-                    <li><a href="/index.php?lang=en">ENG</a></li>
+<?php
+if(isset($_SESSION['lingua']))
+    if($_SESSION['lingua'] == 'it')
+    	echo '<li><a href="/index.php?lang=it" class="current">ITA</a></li><li><a href="/index.php?lang=en">ENG</a></li>';
+    else
+    	echo '<li><a href="/index.php?lang=it">ITA</a></li><li><a href="/index.php?lang=en" class="current">ENG</a></li>';
+else
+	echo '<li><a href="/index.php?lang=it">ITA</a></li><li><a href="/index.php?lang=en">ENG</a></li>';
+?>
+
+
                 </ul>
             </header>
