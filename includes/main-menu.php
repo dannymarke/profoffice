b368e2956418721df9c93438a53c78a2a9335bf7
dannymarke <skylark@iol.it>
2012-11-02 13:09:07 +0100

    commentato link a Gallery ( TBD )

--- includes/main-menu.php
+++ includes/main-menu.php
@@ -63,7 +63,7 @@
                             </ul>
                         </li>
                         <li><a href="/news"><?php echo(strtoupper(NEWS));?></a></li>
-                        <li><a href="/gallery"><?php echo(strtoupper(GALLERIA));?></a></li>
+                        <!--li><a href="/gallery"><?php /*echo(strtoupper(GALLERIA));*/?></a></li-->
                         <li><a href="/referenze"><?php echo(strtoupper(REFERENZE));?></a></li>
                         <li><a href="/contatti"><?php echo(strtoupper(CONTATTI));?></a></li>
                         <li><a href="/login-registrati"><?php echo(strtoupper(LOGIN));?></a></li>
