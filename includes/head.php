ee3c9c6dd2f78129eea261345534df25eacb1212
dannymarke <skylark@iol.it>
2012-11-02 11:28:38 +0100

    settaggi per iPad e iPhone

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="initial-scale=1, width=device-width">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
