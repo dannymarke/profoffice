7dc98c1b9ad392efb3de68e0f634248cc4abd9d1
dannymarke <skylark@iol.it>
2012-11-02 12:22:34 +0100

    struttura di base per pagina contatti

---
+++ contatti.php
@@ -0,0 +1,49 @@
+<?php include_once "includes/conditional-init.php"; ?>
+    <head>
+        <?php include_once "includes/head.php"; ?>
+
+        <link rel="image_src" href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME']; ?>/img/zoom_image.jpg" />
+
+        <meta property="og:image" content="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME']; ?>/img/zoom_image.jpg" />
+        <meta property="og:title" content="Profoffice S.r.l." />
+        <meta property="og:description" content="Profoffice S.r.l. Azienda mobili per arredamento di uffici" />
+
+        <meta name="description" content="Dall'esperienza nel settore del mobile nel 1995 nasce Prof come azienda che produce sistemi d'arredo per ufficio. Partendo dalle collezioni operative, negli anni sviluppa sistemi direzionali, soluzioni per la reception e le sale riunioni, fino alle pareti divisorie e attrezzate. Dal mercato nazionale, Prof si affacciata a quello internazionale con la capacità di proporre soluzioni di arredo complete per tutti gli ambienti di lavoro e con la flessibilità di realizzare progetti su misura personalizzati. Azienda giovane e dinamica, oggi Prof decide di orientare i suoi nuovi prodotti al design più internazionale, con un occhio sempre attento alla funzionalità e alla cura del dettaglio. Oggi Prof si presenta con l'evoluzione del proprio catalogo e con una rafforzata corporate identity, per aumentare la competitività e interpretare il cambiamento delle nuove esigenze dello spazio ufficio contemporaneo."/>
+        <meta name="abstract" content="Profoffice S.r.l. Azienda mobili per arredamento di uffici"/>
+        <meta name="keywords" content="Armadi, Scrivanie, Tavoli riunionr, Parete divisoria, Monolitica, Workstation, Partition, Wall, Cabinets, Desks"/>
+
+        <title>ProfOffice S.r.l. - contatti</title>
+
+    </head>
+    <body class="contatti_wr">
+        <?php include_once "includes/chrome-frame.php"; ?>
+        <div id="main_wr">
+            <?php include_once "includes/main-header.php"; ?>
+            <div id="main_body_wr">
+                <?php include_once "includes/main-sidebar.php"; ?>
+                <section id="main_content_wr" class="bg_zoom dyn_bg_wr">
+                    <div id="left_wr">
+                        <div id="text_wr">
+                            <h2 id="product_name">Uffici, Stabilimento e Showroom:</h2>
+                            <article id="product_description_wr">
+                                <p id="intro_text">
+                                    Via Cao de Villa 6/A <br/>
+                                    31020 Falz&egrave; di Piave, TV / Italy <br/>
+                                    T. <a href="tel:+390438903190" class="tel_num">+39 0438 903190</a> <br/>
+                                    F. <a href="tel:+390438903228" class="tel_num">+39 0438 903228</a> <br/>
+                                    <a class="email_link" href="mailto:info@profoffice.it">info@profoffice.it</a>
+                                </p>
+                                <a class="map_link" href="https://maps.google.it/maps?q=profoffice&daddr=Via+Cao+De+Villa,+6/a,+31020+Treviso+%28Prof+s.r.l.%29&hl=it&ll=45.857888,12.159934&spn=0.012389,0.027852&sll=43.036776,12.392578&sspn=26.584077,57.041016&view=map&geocode=Cah721jhtlHdFRDCuwIdXYS5ACGu8EplzYF8VQ&t=h&z=16&vpsrc=0" target="_blank">Indicazioni stradali</a>
+                            </article>
+                        </div>
+                    </div>
+                    <div id="right_wr" class="dyn_bg_wr">
+                        Per informazioni invia una mail a <a class="email_link" href="mailto:info@profoffice.it">info@profoffice.it</a>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <?php include_once "includes/scripts.php"; ?>
+        <?php include_once "includes/analytics.php"; ?>
+    </body>
+</html>
